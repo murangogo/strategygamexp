cecac6e940b2da33426aabd0731e34a987b6c91d
yanghaotian <448117151@qq.com>
2023-12-06 19:45:10 +0800

    abab

--- api/updatecount.js
+++ api/updatecount.js
@@ -1,19 +1,31 @@
 import db from './dbconnect';
 
 export default async function handler(request, response) {
-  try {
-    const permission = request.query.permission;
-    const result = await db.oneOrNone("SELECT pw FROM ps WHERE un = 'yht'");
-    if(permission==result.pw){
-    // 开始一个事务
-    await db.tx(async t => {
-    // 执行更新操作
-    await t.none('UPDATE download SET num = num + 1 WHERE id = $1', [1]);
-    });
+try {
+    const mytype = request.query.type;
+    if(mytype=="check"){
+        const result = await db.oneOrNone('SELECT num FROM download WHERE id = $1', [1]);
+        if (result==null) {
+            return response.status(500).json({ error: '禁止访问' });
+          }
+          const mynum = result.num;
+          return response.status(200).json({ mynum });
+    }else if(mytype=="update"){
+        const permission = request.query.permission;
+        const result = await db.oneOrNone("SELECT pw FROM ps WHERE un = 'yht'");
+        if(permission==result.pw){
+        // 开始一个事务
+        await db.tx(async t => {
+        // 执行更新操作
+        await t.none('UPDATE download SET num = num + 1 WHERE id = $1', [1]);
+        });
+        }else{
+        throw new Error('禁止访问。');
+        }
+        return response.status(200).json({ message: '更新成功' });
     }else{
-    throw new Error('禁止访问。');
+        throw new Error('禁止访问。');
     }
-    return response.status(200).json({ message: '更新成功' });
   } catch (error) {
     return response.status(500).json({ error: error.message });
   }
